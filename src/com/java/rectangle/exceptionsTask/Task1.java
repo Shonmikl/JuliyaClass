afbf49704c775105e644daa917449cbd892495a8
shonmikl <mikhailegorov676@gmail.com>
2022-07-06 15:06:25 +0300

    -

--- src/com/java/rectangle/exceptionsTask/Task1.java
+++ src/com/java/rectangle/exceptionsTask/Task1.java
@@ -3,42 +3,46 @@ package com.java.rectangle.exceptionsTask;
 import java.util.*;
 
 public class Task1 {
-
     private final static int ZERO = 0; // 0 < a < b
     private final static int RANGE_B = 1000;
     private static int rangeA;
-    private static final int[] array = new int[4]; //0 1 2 3
-
+    private static final int[] ARRAY = new int[4]; //0 1 2 3
 
     public int[] putToArray() {
         Scanner SCANNER = new Scanner(System.in);
         System.out.println("We have a range from " + ZERO + " to " + RANGE_B);
 
         int count = 0;
-        while (count < array.length) {
-            System.out.println("Please enter a digit in this range:");
-            System.out.println(array.length - count + ": numbers left");
+        while (count < ARRAY.length) {
+            while (true) {
+                System.out.println("Please enter a digit in this range: from 0 to " + RANGE_B);
+                System.out.println(ARRAY.length - count + ": numbers left");
 
-            try {
-                rangeA = SCANNER.nextInt();
-            } catch (InputMismatchException e) {
-                System.out.println("You have to put only digit" +
-                        "in range from 0 to " + RANGE_B + "!!!");
+                try {
+                    rangeA = SCANNER.nextInt();
+                } catch (InputMismatchException e) {
+                    System.out.println("You have to put only digit" +
+                            " in range from 0 to " + RANGE_B + "!!!");
+                    break;
+                }
+
+                if (0 < rangeA && rangeA < RANGE_B) {
+                    ARRAY[count] = rangeA;
+                    count++;
+                } else {
+                    System.out.println("You have to use only this range!");
+                }
             }
-
-            if(0 < rangeA && rangeA < RANGE_B) {
-                array[count] = rangeA;
-                count++;
-            } else throw new IndexOutOfBoundsException();
         }
+
         System.out.println("The array was filled");
         SCANNER.close();
 
-        return array;
+        return ARRAY;
     }
 
     public int[] getMinAndMax(int[] array) {
         Arrays.sort(array);
-        return new int[] {array[0], array[array.length-1]};
+        return new int[]{array[0], array[array.length - 1]};
     }
 }
--- src/com/java/rectangle/exceptionsTask/Task1Main.java
+++ src/com/java/rectangle/exceptionsTask/Task1Main.java
@@ -5,9 +5,11 @@ import java.util.Arrays;
 public class Task1Main {
     public static void main(String[] args) {
         Task1 task1 = new Task1();
+        int [] result = task1.putToArray();
+        //System.out.println(Arrays.toString(result));
         System.out.println
                 (Arrays.toString
                         (task1.getMinAndMax
-                                (task1.putToArray())));
+                                (result)));
     }
 }
